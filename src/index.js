48cd3016b3adc655900a6ee651c5395cfc7221d5
ysh <hhhh424@naver.com>
2024-10-10 09:52:27 +0900

    break-tests 오류 복구

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 //version 3
 function factorial(n) {
 
-    //   if( n < 0 ) {
-    //       throw new Error('n must be a positive integer');
-    //   }
+      if( n < 0 ) {
+          throw new Error('n must be a positive integer');
+      }
   
   
       if( n === 0 ) {
